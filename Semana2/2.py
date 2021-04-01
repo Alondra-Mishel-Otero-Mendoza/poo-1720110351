e4c03156d31ebdd92a6c5da62e45e33f2825e176
ALONDRA MISHEL MENDOZA <1720110351@utectulancingo.edu.mx>
2021-04-01 02:16:40 +0000

    Semana 2

---
+++ Semana2/2.py
@@ -0,0 +1,57 @@
+class Estudiante():
+    nombre = None
+    indagadores = None
+    pensador = None
+    comunicantes = None
+    matricula = None
+    cuatrimestre = None
+    uniforme = None
+    mentalidad = None
+    reflexivo = None
+    integro = None
+    
+    
+    def __init__(self):
+        print ("Estudiante")
+    
+    def Tareas(self):
+        print("Realiza")
+    
+    def Estudiar(self):
+        print("Libros")
+    
+    def Aprender(self):
+        print("Materias")
+    
+    def Respetar(self):
+        print("A sus mayores")
+    
+    def Participar(self):
+        print("En clase")
+
+
+
+mishel= Estudiante()
+mishel.nombre=("Alondra Mishel Otero Mendoza")
+mishel.indagadores=("Nivel Medio")
+mishel.pensador=("Nivel Alto")
+mishel.comunicantes=("Influencer")
+mishel.matricula=("1720110358")
+mishel.cuatrimestre=("2 Cuatrimestre")
+mishel.uniforme=("Completo")
+mishel.mentalidad=("Madura")
+mishel.reflexivo=("Si")
+mishel.integro=("Si")
+
+
+
+print(mishel.nombre)
+print(mishel.indagadores)
+print(mishel.pensador)
+print(mishel.comunicantes)
+print(mishel.matricula)
+print(mishel.cuatrimestre)
+print(mishel.uniforme)
+print(mishel.mentalidad)
+print(mishel.reflexivo)
+print(mishel.integro)
