f26b1a2f1ec7e17e724ed9c1b59f8e0d091dd6b5
ALONDRA MISHEL MENDOZA <1720110351@utectulancingo.edu.mx>
2021-04-01 02:28:09 +0000

    ejercicio 4

---
+++ Semana2/4.py
@@ -0,0 +1,57 @@
+class Vacaciones():
+    lugar = None
+    dinero = None
+    trasporte = None
+    persona = None
+    ubicacion = None
+    fauna = None
+    clima = None
+    equipaje = None
+    hotel = None
+    atracciones = None
+    
+    
+    def __init__(self):
+        print ("Avion Militar")
+    
+    def viajar(self):
+        print("¿Con quien?")
+    
+    def Disfrutar(self):
+        print("Del lugar")
+    
+    def Lugares(self):
+        print("¿Donde?")
+    
+    def Distraerse(self):
+        print("Conociendo")
+    
+    def Relajacion(self):
+        print("Ayuda")
+
+ 
+
+playa= Vacaciones()
+playa.lugar=("Playa")
+playa.dinero=("$10,000")
+playa.trasporte=("1")
+playa.persona=("5")
+playa.ubicacion=("Veracruz")
+playa.fauna=("50/100")
+playa.clima=("Caluroso")
+playa.equipaje=("Para playa")
+playa.hotel=("La costa")
+playa.atracciones=("Mar")
+
+
+
+print(playa.lugar)
+print(playa.dinero)
+print(playa.trasporte)
+print(playa.persona)
+print(playa.ubicacion)
+print(playa.fauna)
+print(playa.clima)
+print(playa.equipaje)
+print(playa.hotel)
+print(playa.atracciones)
